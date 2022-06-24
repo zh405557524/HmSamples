16db3636c32e9d12bbad2154f730a0c2b5103a37
zhuming <405557524@qq.com>
2022-06-24 09:29:10 +0800

    hmlib:feat:创建工程

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# hmlib
+# HmSamples
+
+## 一、目录说明。
+
+* 1、lib-鸿蒙lib库
+
---
+++ lib/entry/src/main/java/com/soul/hmlib/MainAbility.java
@@ -0,0 +1,13 @@
+package com.soul.hmlib;
+
+import com.soul.hmlib.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
